8f5b2013764adf6b78e84c4b40bd6942ee9899c1
YanaKhlusova <148796718+YanaKhlusova@users.noreply.github.com>
2025-11-25 16:15:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-print('Hello world')
+"""
+Запуск:
+uvicorn app:app --reload
+
+После открываем в браузере:
+http://localhost:8000/video
+"""
+
+from fastapi import FastAPI, status
+from fastapi.responses import RedirectResponse
+
+app = FastAPI(
+    title="Hallee-Hallo",
+    description="Заглушка приложения для проекта по DL"
+)
+
+VIDEO_URL = "https://rutube.ru/video/c6cc4d620b1d4338901770a44b3e82f4/"
+
+@app.get("/video", summary="MP4 Redirect")
+@app.head("/video", summary="MP4 Redirect (HEAD)")
+
+def redirect_to_video():
+    return RedirectResponse(
+        url=VIDEO_URL,
+        status_code=status.HTTP_302_FOUND,
+    )
